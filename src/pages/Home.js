1607ad406aaabb0b518b96962ae2f9b68fa38124
khamliche-jnAvzu <khamliche@gmail.com>
2023-02-17 18:23:12 +0000

    fix minorbugs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import { Box } from '@mui/material';
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import Exercises from "../components/Exercises";
+import SearchExercises from "../components/SearchExercises";
+import HeroBanner from "../components/HeroBanner";
 
-import Exercises from '../components/Exercises';
-import SearchExercises from '../components/SearchExercises';
-import HeroBanner from '../components/HeroBanner';
-
-const Home = () => {  
-  const [bodyPart, setBodyPart] = useState('all');
+const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
   const [exercises, setExercises] = useState([]);
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises 
-      setExercises={setExercises}
-      bodyPart={bodyPart} 
-      setBodyPart={setBodyPart}
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
       />
       <Exercises
         setExercises={setExercises}
-        bodyPart={bodyPart} 
-        setBodyPart={setBodyPart}/>
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </Box>
   );
 };
