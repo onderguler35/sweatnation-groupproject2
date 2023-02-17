bab7b7810a39061522b0f4a7b2aaf579f27788be
honeylemonrepo <hmi.csbootcamp@gmail.com>
2023-02-17 19:06:17 +0000

    logo upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Box } from '@mui/material';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
 
-import './App.css';
-import ExerciseDetail from './pages/ExerciseDetails';
-import Home from './pages/Home';
-import ContactUs from './pages/ContactUs';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import "./App.css";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import ContactUs from "./pages/ContactUs";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => (
-  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+  <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
@@ -21,4 +21,4 @@ const App = () => (
   </Box>
 );
 
-export default App
+export default App;
