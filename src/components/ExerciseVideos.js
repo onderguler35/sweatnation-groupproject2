71d43286398de484c4602d55fe829ac5d94f5388
Gatsby <115715220+onderguler35@users.noreply.github.com>
2023-02-18 19:30:46 +0000

    some minor corrections

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,14 +1,30 @@
 import React from "react";
-import { Typography, Box, Stack } from '@mui/material';
-import Loader from './Loader';
+import { Typography, Box, Stack } from "@mui/material";
+import Loader from "./Loader";
 
 const ExerciseVideos = ({ exerciseVideos, name }) => {
   if (!exerciseVideos.length) return <Loader />;
-<Box sx={{ marginTop: { lg: '203px', xs: '20px' } }} p="20px">
-      <Typography sx={{ fontSize: { lg: '44px', xs: '25px' } }} fontWeight={700} color="#000" mb="33px">
-        Watch <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>{name}</span> exercise videos
+
+  return (
+    <Box sx={{ marginTop: { lg: "203px", xs: "20px" } }} p="20px">
+      <Typography
+        sx={{ fontSize: { lg: "44px", xs: "25px" } }}
+        fontWeight={700}
+        color="#000"
+        mb="33px"
+      >
+        Watch{" "}
+        <span style={{ color: "#FF2625", textTransform: "capitalize" }}>
+          {name}
+        </span>{" "}
+        exercise videos
       </Typography>
-      <Stack sx={{ flexDirection: { lg: 'row' }, gap: { lg: '110px', xs: '0px' } }} justifyContent="flex-start" flexWrap="wrap" alignItems="center">
+      <Stack
+        sx={{ flexDirection: { lg: "row" }, gap: { lg: "110px", xs: "0px" } }}
+        justifyContent="flex-start"
+        flexWrap="wrap"
+        alignItems="center"
+      >
         {exerciseVideos?.slice(0, 3)?.map((item, index) => (
           <a
             key={index}
@@ -17,9 +33,17 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
             target="_blank"
             rel="noreferrer"
           >
-            <img style={{ borderTopLeftRadius: '20px' }} src={item.video.thumbnails[0].url} alt={item.video.title} />
+            <img
+              style={{ borderTopLeftRadius: "20px" }}
+              src={item.video.thumbnails[0].url}
+              alt={item.video.title}
+            />
             <Box>
-              <Typography sx={{ fontSize: { lg: '28px', xs: '18px' } }} fontWeight={600} color="#000">
+              <Typography
+                sx={{ fontSize: { lg: "28px", xs: "18px" } }}
+                fontWeight={600}
+                color="#000"
+              >
                 {item.video.title}
               </Typography>
               <Typography fontSize="14px" color="#000">
@@ -30,9 +54,6 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
         ))}
       </Stack>
     </Box>
-  return(
-
-  )
+  );
 };
-
 export default ExerciseVideos;
